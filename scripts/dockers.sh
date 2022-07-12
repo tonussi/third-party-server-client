123c8daf45113ac62f1546894092b042b2cb9648
Lucas <lucas@prevision.com.br>
2022-07-12 00:39:00 -0300

    Adjust docker

--- scripts/dockers.sh
+++ scripts/dockers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 DOCKERHUB_USER_NAME=lptonussi
 
-docker build -t $DOCKERHUB_USER_NAME/http-arangodb-client -f http-arangodb-client/Dockerfile .
+docker build -t $DOCKERHUB_USER_NAME/http-arangodb-client -f http-arangodb-client/Dockerfile ./http-arangodb-client
 
 docker push $DOCKERHUB_USER_NAME/http-arangodb-client
